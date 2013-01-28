390d393e762f55bccf651407f85e8259f1d21a8e
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-01-28 12:02:28 +0100

    Added comment on A and B

--- SingletonPrototype/A.cs
+++ SingletonPrototype/A.cs
@@ -1,8 +1,11 @@
 ﻿namespace SingletonPrototype
 {
+    /// <summary>
+    ///  True singleton with private constructor
+    /// </summary>
     public class A: SingletonBase<A>
     {
-        private A(){}
+        private A(){} 
 
         public string Print()
         {
--- SingletonPrototype/B.cs
+++ SingletonPrototype/B.cs
@@ -1,5 +1,8 @@
 ﻿namespace SingletonPrototype
 {
+    /// <summary>
+    /// Not really true singleton. Has default public constructor;
+    /// </summary>
     public class B : SingletonBase<B>
     {
         public string Print()
